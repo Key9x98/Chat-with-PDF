5a7421efb2d673614c8a1fab359b3ff736753c35
Hoang Vu Minh <hoang.vuminh1512@gmail.com>
2024-07-25 09:30:02 +0700

    update chat.py

---
+++ chat.py
@@ -0,0 +1,83 @@
+import google.generativeai as genai
+import os
+
+gemini_api_secret_name = 'gemini-pro'  # @param {type: "string"}
+model = genai.GenerativeModel('gemini-pro')
+
+from dotenv import load_dotenv
+
+# Load .env file
+load_dotenv()
+
+GOOGLE_API_KEY = os.getenv("API_KEY")
+MODEL_NAME =  os.getenv("MODEL_NAME")
+print(MODEL_NAME)
+
+generation_config = {
+  "temperature": 0.05,
+  "top_p": 1,
+  "top_k": 1,
+  "max_output_tokens": 2048,
+}
+
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+]
+
+
+class GeminiBot:
+  def __init__(self):
+    self.model_name = MODEL_NAME
+    self.token_count = None
+    self.model = None
+    self.chat = None
+    self._setup()
+
+  def _setup(self):
+    genai.configure(api_key=GOOGLE_API_KEY)
+    PROMPT = 'Ban là một công cụ chat, hãy trả lời các câu hỏi của người dùng'
+    self.model = genai.GenerativeModel(model_name=self.model_name,
+                                       generation_config=generation_config,
+                                       safety_settings=safety_settings)
+                                       # system_instruction=PROMPT)
+    self.token_count = self.model.count_tokens(PROMPT).total_tokens
+    self.chat = self.model.start_chat()
+
+
+  def response(self, user_input):
+    user_input = user_input
+
+    input_tokens = self.model.count_tokens(user_input).total_tokens
+
+    if self.token_count > 1000000:
+      return "Out of tokens"
+
+    prompt = "Bạn là một hệ thống hỏi đáp, hãy giúp người dùng trả lời câu hỏi theo cú pháp câu hỏi - câu trả lời"
+
+    response = self.chat.send_message(prompt + user_input)
+    self.token_count += input_tokens + self.model.count_tokens(response.text).total_tokens
+
+    return response.text
+
+
+gemini_bot = GeminiBot()
+
+# Sử dụng đối tượng để trả lời câu hỏi
+user_question = "1 + 1 bằng mấy"
+bot_response = gemini_bot.response(user_question)
+print(bot_response)
