51d82a4b1e8ff3e5f865390ab31ecac9432007ca
Hoang Vu Minh <150101931+vuminhoang@users.noreply.github.com>
2024-07-26 09:31:47 +0700

    Update embedding.py

--- embedding.py
+++ embedding.py
@@ -1,17 +1,13 @@
 from sentence_transformers import SentenceTransformer
 import torch
 
-class DocumentEmbedder:
+
+class DocumentEmbedder(Embeddings):
     def __init__(self, model_name="hiieu/halong_embedding"):
         self.model = SentenceTransformer(model_name)
     
-    def convert_to_vec(self, docs):
-        """
-        encode single docs / list of docs to vectors
-        """
-        return self.model.encode(docs)
-
-halong = DocumentEmbedder()
-text = "test"
-embed=  halong.convert_to_vec(text)
-print(embed)
+    def embed_documents(self, documents):
+        return self.model.encode(documents)
+    
+    def embed_query(self, query):
+        return self.model.encode([query])[0]
