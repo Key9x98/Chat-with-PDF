52e17ad4e13baca122003e1927e633de515dec30
Key9x98 <90105151+Key9x98@users.noreply.github.com>
2024-07-26 14:38:57 +0700

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ from langchain.chains import RetrievalQA
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from embedding import custom_embeddings
 
 # Đường dẫn tới cơ sở dữ liệu vector
 vector_db_path = "vectorstores/db_faiss"
@@ -41,7 +43,6 @@ safety_settings = [
     },
 ]
 
-# Định nghĩa lớp GeminiBot
 class GeminiBot:
     def __init__(self):
         self.model_name = MODEL_NAME
@@ -56,57 +57,66 @@ class GeminiBot:
         self.model = genai.GenerativeModel(model_name=self.model_name,
                                            generation_config=generation_config,
                                            safety_settings=safety_settings)
-                                           # system_instruction=PROMPT)
-        self.token_count = self.model.count_tokens(PROMPT).total_tokens
+        # self.token_count = self.model.count_tokens(PROMPT).total_tokens
         self.chat = self.model.start_chat()
 
     def response(self, user_input):
-        user_input = user_input
-        input_tokens = self.model.count_tokens(user_input).total_tokens
-        if self.token_count > 1000000:
-            return "Out of tokens"
-        prompt = "Bạn là một hệ thống hỏi đáp, hãy giúp người dùng trả lời câu hỏi theo cú pháp câu hỏi - câu trả lời"
-        response = self.chat.send_message(prompt + user_input)
-        self.token_count += input_tokens + self.model.count_tokens(response.text).total_tokens
+        # input_tokens = self.model.count_tokens(user_input).total_tokens
+        # if self.token_count > 1000000:
+        #     return "Out of tokens"
+        response = self.chat.send_message(user_input)
+        # self.token_count += input_tokens + self.model.count_tokens(response.text).total_tokens
         return response.text
 
 # Đọc cơ sở dữ liệu vector
 def read_vectors_db():
-    embedding_model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    embedding_model = custom_embeddings
     db = FAISS.load_local(vector_db_path, embedding_model, allow_dangerous_deserialization=True)
     return db
+
+custom_prompt_template = """Sử dụng các thông tin sau đây để trả lời câu hỏi của người dùng.
+Nếu bạn không biết câu trả lời, chỉ cần nói rằng bạn không biết, đừng cố bịa ra câu trả lời.
+Tất cả câu trả lời của bạn đều phải trả lời bằng tiếng việt
+
+Context: {context}
+Question: {question}
+"""
+
+def set_custom_prompt():
+    """
+    Prompt template for QA retrieval for each vectorstore
+    """
+    prompt = PromptTemplate(template=custom_prompt_template,
+                            input_variables=['context', 'question'])
+    return prompt
 
 # Tạo hàm để trả lời câu hỏi dựa vào cơ sở dữ liệu vector
 def answer_question(question):
     # Đọc cơ sở dữ liệu vector
     db = read_vectors_db()
     retriever = db.as_retriever()
-    # embedding_model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-    query = question
-    documents = retriever.get_relevant_documents(query)
 
-    # Tạo instance của GeminiBot
-    # gemini_bot = GeminiBot()
+    # Retrieve relevant documents
+    documents = retriever.invoke(question)
+    if not documents:
+        return "Không có câu trả lời nào được tìm thấy."
 
-    # # Tạo đối tượng RetrievalQA
-    # retrieval_qa = RetrievalQA.from_chain_type(
-    #     llm = gemini_bot.model,
-    #     retriever=retriever,
-    #     combine_documents_chain=gemini_bot.response,  # Sử dụng hàm response của GeminiBot
-    #     return_source_documents=True
-    # )
-
-    # # Trả lời câu hỏi
-    # result = retrieval_qa({"query": question})
-    # answer = result.get("answer", "Không có câu trả lời nào được tìm thấy.")
-    gemini_bot = GeminiBot()
-    prompt = """answer my question"""
-    ans = gemini_bot.response(prompt+question+str(documents[0]))
-    print(documents[0])
+    # Create context from documents
+    context = "\n\n".join([doc.page_content for doc in documents])
     print("="*25)
-    return ans
+    print(context)
+    print("="*25)
+    # Set custom prompt
+    custom_prompt = set_custom_prompt()
+    prompt_context = custom_prompt.format(context=context, question=question)
+
+    gemini_bot = GeminiBot()
+    answer = gemini_bot.response(prompt_context)
+    
+    return answer
 
 # Ví dụ sử dụng
-user_question = "What you will learn in Sentiment Analysis with LSTM and TorchText with Code and Explanation Article?"
+user_question = "Tác giả của bài báo Thực hiện song song thuật toán AES bằng ngôn ngữ lập trình CUDA trên GPU NVIDIA là ai?"
 answer = answer_question(user_question)
 print(answer)
+print("="*25)
