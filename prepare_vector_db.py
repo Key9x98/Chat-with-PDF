92bb6f2a8f4446a9d3698a4f11104f116105cf66
Key9x98 <90105151+Key9x98@users.noreply.github.com>
2024-07-29 13:03:44 +0700

    Update prepare_vector_db.py

--- prepare_vector_db.py
+++ prepare_vector_db.py
@@ -1,24 +1,57 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from sentence_transformers import SentenceTransformer
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.docstore.document import Document
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
 from embedding import custom_embeddings
+from typing import List
 # Khai bao bien
 pdf_data_path = "data"
 vector_db_path = "vectorstores/db_faiss"
 
+def text_splitter(text: str) -> List[str]:
+    chunks = []
+    paragraphs = text.split('\n')
+    current_chunk = []
+    current_word_count = 0
+
+    for paragraph in paragraphs:
+        words = paragraph.split()
+        if current_word_count + len(words) >= 100:
+            if current_chunk:
+                chunks.append(' '.join(current_chunk))
+            current_chunk = words
+            current_word_count = len(words)
+        else:
+            current_chunk.extend(words)
+            current_word_count += len(words)
+    if current_chunk:
+        chunks.append(' '.join(current_chunk))
+    
+    return chunks
+
+
+#seperator tốt: "\n\n", "\n", " ", ".", "!", "?",""
+#chunk_size = 1024, chunk_overlap = 64
 def create_db_from_files():
     # Khai bao loader de quet toan bo thu muc dataa
     loader = DirectoryLoader(pdf_data_path, glob="*.pdf", loader_cls = PyPDFLoader)
     documents = loader.load()
-
     text_splitter = RecursiveCharacterTextSplitter(
-        separators="\n",
-        chunk_size=512, 
-        chunk_overlap=128,
-        length_function=len)
-    chunks = text_splitter.split_documents(documents)   git
+        separators=[
+            "\n\n", 
+            "\n", 
+            " ", 
+            ".", 
+            "!", 
+            "?",
+            ""
+        ],
+        chunk_size=1024,
+        chunk_overlap=64
+    )
+    chunks = text_splitter.split_documents(documents)
     # Create document objects from chunks
     # documents = [Document(page_content=chunk.page_content) for chunk in chunks]
     # Embedding
