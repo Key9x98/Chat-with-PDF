75dac61eaa81dd420b93e1a8c6dbbcad01e2326d
Key9x98 <90105151+Key9x98@users.noreply.github.com>
2024-08-22 11:10:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -30,9 +30,13 @@ def main():
 
     st.markdown(
     """
-    #GithubIcon {
-      visibility: hidden;
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
     }
+    </style>
     """,
     unsafe_allow_html=True
     )
